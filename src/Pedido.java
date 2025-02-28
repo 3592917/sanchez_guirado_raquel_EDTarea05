91f978ddd2014984e41929830eb407ef3b67754d
3592917 <3592917@alu.murciaeduca.es>
2025-02-28 18:14:00 +0100

    Refactorización de las clases cliente y pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,25 +1,36 @@
 public class Pedido {
     public Cliente cliente;
+    private double descuento;
+    private double precioFinal;
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
     public Pedido(Cliente cliente) {
         this.cliente = cliente;
     }
 
-    public void procesarPedido(double total) {
+    public void procesarPedido(double total){
         if (total > 100) {
-            double descuento = total * 0.10;
-            double precioFinal = total - descuento;
-            System.out.println("Pedido aprobado. Total: " + precioFinal + " (descuento aplicado: " + descuento + ")");
+            descuento = total * (cliente.isEsVip() ? 0.15 : 0.10);
+            precioFinal = total - descuento;
+            System.out.println("Pedido " + (cliente.isEsVip() ? "VIP" : "") + " aprobado. Total: "
+                    + precioFinal + " (descuento aplicado: " + descuento + ")");
         } else {
-            System.out.println("Pedido aprobado. Total: " + total);
-        }
-    }
-    public void procesarPedidoVIP(double total) {
-        if (total > 100) {
-            double descuento = total * 0.15;
-            double precioFinal = total - descuento;
-            System.out.println("Pedido VIP aprobado. Total: " + precioFinal + " (descuento aplicado: " + descuento + ")");
-        } else {
-            System.out.println("Pedido VIP aprobado. Total: " + total);
+            System.out.println("Pedido " + (cliente.isEsVip() ? "VIP" : "") + " aprobado. Total: " + total);
         }
     }
 }
